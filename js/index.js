a273782d6fbd78c9d4018d152e4f8d7dfb5d3f71
furqanshabbirahmedabbasi <furqansonu377@gmail.com>
2025-11-03 23:39:50 +0500

    initail push

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const book_appiontment_page = document.getElementById('book_appiontment_page');
+
+book_appiontment_page.addEventListener('click', () => {
+    window.location.href='book_appointment.html'
+})
